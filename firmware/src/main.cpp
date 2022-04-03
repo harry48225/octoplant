0ee7a6cb99feb64497df0e0935cb9091219c71b7
Harry Best <harrybest800@gmail.com>
2022-04-03 17:39:24 +0100

    Basic button test

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -4,6 +4,8 @@
 
 #define MILLIS_USE_TIMERB0 // Use timer B for millis - we use timer A for PWM and the leds
 
+volatile byte ledState = 0;
+
 void setup() {
   takeOverTCA0();
   TCA0.SPLIT.CTRLD = 0x01; // Enable split mode  
@@ -12,6 +14,8 @@ void setup() {
   MoistureManager::setup();
 
   TCA0.SPLIT.CTRLA = TCA_SPLIT_ENABLE_bm | TCA_SPLIT_CLKSEL_DIV64_gc; //enable the timer 64 prescaler
+
+  PORTA.PIN2CTRL |= (PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc); // Enable pin change interrupt and internal pullup
 }
 
 void loop() {
@@ -24,4 +28,16 @@ void loop() {
       LedManager::turnOffLed(i);
     }
   }
+
+  if (ledState) {
+    LedManager::turnOnLed(2);
+  } else {
+    LedManager::turnOffLed(2);
+  }
+}
+
+ISR(PORTA_PORT_vect) {
+  byte flags = PORTA.INTFLAGS;
+  PORTA.INTFLAGS = flags; // Clear the int flags
+  ledState = !ledState;
 }
